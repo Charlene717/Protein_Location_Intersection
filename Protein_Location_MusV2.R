540d599411a95d5491178aeb4aa439f4ee68de41
Ch_HCC <q2330416@gmail.com>
2021-04-21 18:58:43 +0800

    Version folder

--- Protein_Location_MusV2.R
+++ Protein_Location_MusV2.R
@@ -4,6 +4,9 @@ rm(list = ls()) #清除變數
 setwd(getwd()) ## Set current working directory
 PathName <- getwd() ## Set output directroy
 #
+RVersion = "20210421V1"
+dir.create(paste0(PathName,"/",RVersion))
+
 datatext <- read.table(paste0(PathName,"/mouse_compartment_textmining_full.tsv"),  # 資料檔名
                       header=F,          # 資料中的第一列，作為欄位名稱
                       sep="\t")           # 將逗號視為分隔符號來讀取資料
@@ -65,13 +68,13 @@ datainte_cyto4 <- setDT(datainte_cyto3)[, .SD[1], by = .(V2)]
 #OK# 
 datainte_cyto5 = unique(datainte_cyto3, by = "V2")
 
-write.table(datainte_cyto3, file=paste0(PathName,"/cyto_MusTTT.txt"),  
+write.table(datainte_cyto3, file=paste0(PathName,"/",RVersion,"/cyto_MusTTT.txt"),  
             sep="\t", row.names=FALSE)
 
-write.table(datainte_cyto4, file=paste0(PathName,"/cyto_Mus.txt"),  
+write.table(datainte_cyto4, file=paste0(PathName,"/",RVersion,"/cyto_Mus.txt"),  
             sep="\t", row.names=FALSE)
 
-write.table(datainte_cyto4, file=paste0(PathName,"/cyto_Mus.csv"),  
+write.table(datainte_cyto4, file=paste0(PathName,"/",RVersion,"/cyto_Mus.csv"),  
             sep=",", row.names=FALSE)
 
 
@@ -79,16 +82,17 @@ write.table(datainte_cyto4, file=paste0(PathName,"/cyto_Mus.csv"),
 datainte_extra <- datainte[datainte[,5] >= 2,]
 datainte_extra2_1 <- datainte_extra[grepl("extracellular", datainte_extra[,4], ignore.case=TRUE),]
 datainte_extra2 <- datainte_extra2_1[!grepl("exosome", datainte_extra2_1[,4], ignore.case=TRUE),]
+datainte_extra2 <- datainte_extra2_1[!grepl("vesicle", datainte_extra2_1[,4], ignore.case=TRUE),]
 datainte_extra2 <- datainte_extra2[!grepl("mmu-miR-", datainte_extra2[,2], ignore.case=TRUE),]
 
 datainte_extra3 <- datainte_extra2[ ,c(2,5)]
 #datainte_extra3 <- as.matrix(datainte_extra3)
 datainte_extra4 <- setDT(datainte_extra3)[, .SD[1], by = .(V2)] 
 
-write.table(datainte_extra4, file=paste0(PathName,"/Extracellular_Mus.txt"),  
+write.table(datainte_extra4, file=paste0(PathName,"/",RVersion,"/Extracellular_Mus.txt"),  
             sep="\t", row.names=FALSE)
 
-write.table(datainte_extra4, file=paste0(PathName,"/Extracellular_Mus.csv"),  
+write.table(datainte_extra4, file=paste0(PathName,"/",RVersion,"/Extracellular_Mus.csv"),  
             sep=",", row.names=FALSE)
 
 
@@ -106,30 +110,30 @@ CytoGene2 <- AnnotationDbi::select(org.Mm.eg.db, keys=CytoGene, columns='ENSEMBL
 CytoGene3 <- CytoGene2[!is.na(CytoGene2$ENSEMBL), ]
 CytoGene3 <- unique(CytoGene3, by = "ENSEMBL")
 
-write.table(CytoGene3, file=paste0(PathName,"/CytoGene_Mus_ENSEMBL.csv"),  
+write.table(CytoGene3, file=paste0(PathName,"/",RVersion,"/CytoGene_Mus_ENSEMBL.csv"),  
             sep=",", row.names=FALSE)
 
 CytoGene4 <- CytoGene3[,2]
 CytoGene4 <- AnnotationDbi::select(org.Mm.eg.db, keys=CytoGene4, columns='SYMBOL', keytype='ENSEMBL') 
 CytoGene5 <- CytoGene4[!is.na(CytoGene4$SYMBOL), ]
 CytoGene5 <- unique(CytoGene5, by = "SYMBOL")
-write.table(CytoGene5, file=paste0(PathName,"/CytoGene_Mus_SYMBOL.csv"),  
+write.table(CytoGene5, file=paste0(PathName,"/",RVersion,"/CytoGene_Mus_SYMBOL.csv"),  
             sep=",", row.names=FALSE)
 
 #library(readxl)
 # Error# FNkdGenelise <- read_excel(paste0(PathName,"/FNkd unique.xlsx"), sheet = "工作表1")
 
-FNkdGenelise <- read.csv(paste0(PathName,"/FNkd unique.csv"),header = T)  # 資料檔名
+FNkdGenelise <- read.csv(paste0(PathName,"/",RVersion,"/FNkd unique.csv"),header = T)  # 資料檔名
 FNkdExoGene  <- as.vector(as.matrix(FNkdGenelise[,1]))
 FNkdExoGene2 <- AnnotationDbi::select(org.Mm.eg.db, keys=FNkdExoGene, columns='ENSEMBL', keytype='SYMBOL') 
 FNkdExoGene3 <- FNkdExoGene2[!is.na(FNkdExoGene2$ENSEMBL), ]
 FNkdExoGene3 <- unique(FNkdExoGene3, by = "ENSEMBL")
-write.table(FNkdExoGene3, file=paste0(PathName,"/FNkdExoGene_Mus_ENSEMBL.csv"),  
+write.table(FNkdExoGene3, file=paste0(PathName,"/",RVersion,"/FNkdExoGene_Mus_ENSEMBL.csv"),  
             sep=",", row.names=FALSE)
 
 FNkdExoGene4 <- FNkdExoGene3[,2]
 FNkdExoGene4 <- AnnotationDbi::select(org.Mm.eg.db, keys=FNkdExoGene4, columns='SYMBOL', keytype='ENSEMBL') 
 FNkdExoGene5 <- FNkdExoGene4[!is.na(FNkdExoGene4$SYMBOL), ]
 FNkdExoGene5 <- unique(FNkdExoGene5, by = "SYMBOL")
-write.table(FNkdExoGene5, file=paste0(PathName,"/FNkdExoGene_Mus_SYMBOL.csv"),  
+write.table(FNkdExoGene5, file=paste0(PathName,"/",RVersion,"/FNkdExoGene_Mus_SYMBOL.csv"),  
             sep=",", row.names=FALSE)
